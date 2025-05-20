5f208870f7d49038f6425d29e8e9daf1ec4918db
Bill Van Ark <4hchm4xub2@snkmail.com>
2025-05-20 20:00:16 +0200

    Removed implemented design notes

--- Main.lua
+++ Main.lua
@@ -6,16 +6,6 @@ import "Turbine.UI.Lotro";
 import "CubePlugins.HerbalismHelper.GeneralFunctions";
 import "CubePlugins.HerbalismHelper.Flora";
 
-
---[[
-
-Design notes:
-Pay attention to target
-If target has the name of one of the herbalism flowers,
-    Show window with expected flower output
-
-]]
-
 LocalPlayer = Turbine.Gameplay.LocalPlayer.GetInstance();
 LocalPlayer.TargetChanged = function(sender, args)
     HandleTargetChanged();
