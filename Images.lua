4e287bd52a9035a8ccae0c0c68b11c25c49ed433
Bill Van Ark <4hchm4xub2@snkmail.com>
2025-05-20 21:29:37 +0200

    Added phial image IDs

--- Images.lua
+++ Images.lua
@@ -2,4 +2,11 @@ ResourcesDir = "CubePlugins/HerbalismHelper/Resources/";
 
 _IMAGES = {
     ["TARGET_WINDOW_BACKGROUND"] = ResourcesDir .. "TargetWindowBackground.tga";
+
+    -- Phials:
+    ["PHIAL_CRIMSON_EXTRACT"] = 0x411A2B85;
+    ["PHIAL_UMBER_EXTRACT"] = 0x411A2B7F;
+    ["PHIAL_VERDANT_EXTRACT"] = 0x411A2B7A;
+    ["PHIAL_GOLDEN_EXTRACT"] = 0x411A2B82;
+    ["PHIAL_VIOLET_EXTRACT"] = 0x411A2B84;
 }
